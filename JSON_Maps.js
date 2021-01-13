071efcfd5e05326ce421008732e801b25d3d1f3d
anasebescen <anasebescen@users.noreply.github.com>
2021-01-13 08:14:39 -0300

    Create JSON_Maps.js

---
+++ JSON_Maps.js
@@ -0,0 +1,30 @@
+eqfeed_callback({
+  "type": "EstabelecimentosCollection",
+  "estabelecimentos": [{
+      "type": "Estabelecimento",
+      "properties": {
+        "name": "Restaurante 1",
+        "address": "http://earthquake.usgs.gov/earthquakes/eventpage/usc000csx3",
+      },
+      "geometry": {
+        "type": "restaurante",
+        "coordinates": [126.3832, 5.9775, 111.16],
+      },
+      "id": "restaurante1"
+    },
+    {
+    "type": "EstabelecimentosCollection",
+    "estabelecimentos": [{
+        "type": "Estabelecimento",
+        "properties": {
+          "name": "Restaurante 2",
+          "address": "http://earthquake.usgs.gov/earthquakes/eventpage/usc000csx3",
+        },
+        "geometry": {
+          "type": "restaurante",
+          "coordinates": [-170.0567, 52.4716, 127.2],
+        },
+        "id": "restaurante1"
+      },
+  ]
+});
